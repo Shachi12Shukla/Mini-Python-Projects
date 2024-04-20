6924393681380895c0e9912a0b969cf35773ff50
Shachi Shukla <152056204+Shachi12Shukla@users.noreply.github.com>
2024-04-20 20:20:26 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+
+import langchain
+import ollama
+
+stream = ollama.chat(
+    model = 'mistral',
+    messages= [{'role':'user','content':'Why is the sky blue?'}],
+    stream = True,
+)
+
+for chunk in stream:
+    print(chunk['message']['content'],end = '', flush =True )
